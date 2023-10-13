afa8b5dd8821ac1c65cadb41cf67a5a3109821fa
도경 <lee7198@naver.com>
2023-10-13 12:34:57 +0900

    update basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Observer from "./pages/observer";
 import Main from "./pages/main";
 
-const router = createBrowserRouter([
+const isDEV = process.env.NODE_ENV === "development";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Main />,
+      children: [{ path: "observer", element: <Observer /> }],
+    },
+  ],
   {
-    path: "/",
-    element: <Main />,
-    children: [{ path: "observer", element: <Observer /> }],
-  },
-]);
+    basename: isDEV ? "/" : "/DesignPatternStudy",
+  }
+);
 
 export default function Router() {
   return <RouterProvider router={router} />;
