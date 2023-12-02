83fba0a7874797ac80f341df4041a0a84984b16e
도경 <lee7198@naver.com>
2023-12-02 16:46:11 +0900

    add alt des

--- src/pages/patterns/Proxy/components/Chunsik.tsx
+++ src/pages/patterns/Proxy/components/Chunsik.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import chunsik from "../../../../assets/images/JejuChunsik.png";
 
 export default function Chunsik() {
-  return <img style={{ width: "100%" }} src={chunsik} />;
+  return <img alt="하르방춘식" style={{ width: "100%" }} src={chunsik} />;
 }
